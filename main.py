7a314950c7d71e2694f67bb497f476ea560e38bc
Screaming Opossum <alx@Alxs-MacBook-Air.local>
2025-01-07 14:48:55 +0300

    Updated project with latest changes

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+# Database Configuration
+DATABASE_URL = "postgresql://alx:1uWb7k2Q90g@localhost/coca_coladb"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Model
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+# Create Tables
+Base.metadata.create_all(bind=engine)
+
+# App
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 
 @app.get("/hello/{name}")
